0f9977ef1eb3ed41d0bd061b7ca09f07954a66ab
Saravanan M <sarvan.m20@gmail.com>
2024-11-25 12:01:26 +0530

    cc

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "../styles/navbar.module.css";
 import Link from "next/link";
+import { FaHome } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div className={styles.navbarContainer}>
-      <div className={styles.logo}></div>
+      <div className={styles.logo}>
+        <Link href={"/"}>
+          <FaHome />
+        </Link>
+      </div>
       <div className={styles.links}>
         <Link href={"/about"}>About Us</Link>
         <Link href={"/vision"}>Our Vision</Link>
